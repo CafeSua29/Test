4b41f8cf9c9768f16591114d336da3ddb8a715e7
CafeSua29 <luongbaphananh@gmail.com>
2023-09-29 23:34:22 +0700

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,25 @@
+﻿void inHinhThoi(int rows)
+{
+    if (rows % 2 == 0) return;
+
+    for(int i = 0; i < rows; i++)
+    {
+        int spaces = Math.Abs(rows / 2 - i); 
+
+        int stars = rows - 2 * spaces;
+
+        for(int j = 0; j < spaces; j++)
+        {
+            Console.Write(" ");
+        }
+
+        for(int k = 0;  k < stars; k++)
+        {
+            Console.Write("*");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+inHinhThoi(7);
